5b6e7e4bb499bd9440648daae43c8adc33d9e3b1
micoo-c <69345177+micoo-c@users.noreply.github.com>
2020-08-08 13:41:12 +1200

    Adding Assets folder

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horixontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horixontalInput, 0, 0) * Time.deltaTime * _speed);
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, verticalInput) * Time.deltaTime * _speed);
+    }
+}
