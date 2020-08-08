02202367a0c9ff8f89623b1ffbba226f77c387cd
micoo-c <69345177+micoo-c@users.noreply.github.com>
2020-08-08 19:51:40 +1200

    movement and looking

---
+++ Assets/Scripts/LookAround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookAround();
+    }
+
+    void lookAround()
+    {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.down, Vector3.up);
+        float rayDistance;
+
+        if (ground.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            transform.LookAt(point);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speedH;
+    [SerializeField]
+    private float _speedV;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveAround();
+    }
+
+    void moveAround()
+    {
+        float horixontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horixontalInput, 0, 0) * Time.deltaTime * _speedV);
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(0, 0, verticalInput) * Time.deltaTime * _speedH);
+    }
+}
